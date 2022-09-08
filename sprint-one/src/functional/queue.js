2f66199811a3c541ab519f8145c8f1e2825125a9
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-08 12:29:00 -0700

    pass stack.js and queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var index;
+    if (Object.keys(storage).length === 0) {
+      index = 0;
+    } else {
+      var storageLength = Object.keys(storage).length;
+      if (storage[storageLength]) {
+        storageLength++;
+      }
+      index = storageLength;
+    }
+    storage[index] = value;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var index = keys[0];
+    var dequeued = storage[index];
+    delete storage[index];
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var storageLength = Object.keys(storage).length;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var index;
+    if (Object.keys(storage).length === 0) {
+      index = 0;
+    } else {
+      var index = Object.keys(storage).length;
+    }
+    storage[index] = value;
   };
 
   someInstance.pop = function() {
+    var index = Object.keys(storage).length - 1;
+    var popped = storage[index];
+    delete storage[index];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
