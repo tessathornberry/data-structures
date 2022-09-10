0518cd13bdf2921839d63c0f16fa4462591abae2
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-10 12:35:25 -0700

    pass graph.js pair for Data-Structures with Curtis Wang

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -13,47 +13,76 @@
 // Instantiate a new graph
 //var graph = new Graph();
 var Graph = function() {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  console.log('node ', node); //graph = {{1: connector: 2, 3}, {node: 3}}
-  console.log('this ', this);
-  // if (!this.node) {
-  //   console.log('node again!', node);
-  // }
-  // var newNode = new Graph();
-  // newNode.node = node;
-  this[node] = {'connector': []};
-  // var newNode = Graph
+  this.storage[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.storage[node]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var nodeArray = this.storage[node];
+  if (this.storage[node].length > 0) {
+    for (var i = 0; i < nodeArray.length; i++) {
+      var removing = nodeArray[i];
+      this.removeEdge(removing, node);
+    }
+  }
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.storage[fromNode].includes(toNode)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var startPos = this.storage[fromNode]; //4: [5]
+  var endPos = this.storage[toNode];
+  _.each(startPos, function(element, index) {
+    if (element === toNode) {
+      startPos.splice(index, 1);
+    }
+  });
+  _.each(endPos, function(element, index) {
+    if (element === fromNode) {
+      endPos.splice(index, 1);
+    }
+  });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.storage) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+addNode: , contains: , removeNode: , addEdge: , hasEdge: , removeEdge: , forEachNode:
+*/
 
 
